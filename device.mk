0687e6bc516c0b362eed8c8cb40461e82525f797
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-12-24 16:00:30 +0100

    Apollo: Add fastcharge implementation
    * Disabling fast charge may be useful for reducing the heat produced by
       the device while charging, or for extending the lifespan of the battery
    
     * This commit introduces the fastcharge HIDL, which writes in the node
    
                  /sys/class/qcom-battery/restrict_chg
    
       0 or 1 depeding on user selection.
       0 means that fastcharge is enabled, while 1 means that fastcharge is
         disabled
    
    Change-Id: I369ba9c437b3a83a88c2ce74d603b7d7ddd9cfbb
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Signed-off-by: althafvly <althafvly@gmail.com>
    
    Apollo: rootdir: give permission to restrict_chg
    
    Apollo: Sepolicy: Fastcharge: Provide needed permissions
    
    Apollo: Sepolicy: Label FastCharge more general
    
    Apollo: Build fastcharge implementation
    
    Change-Id: I3e6994d2c2f419dc8ce98a060204fda3dd16df81
    
    Apollo: fastcharge: Migrate to blueprint
    
    Change-Id: I200fd6c685183c98f0815b207adae5aa75a37238
    
    Apollo: fastcharge: Use pragma once
    
    Change-Id: I9a9e193a6f66f045f41394d059af8b71778e8d72
    
    Apollo: fastcharge: Make user setting persistent
    
    Change-Id: I4ff051d6f9685e94022eab32035bfe86a65264b8
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Co-Authored-By: Tofu <ralph950412@gmail.com>

--- device.mk
+++ device.mk
@@ -150,6 +150,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.xiaomi_apollo
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi
