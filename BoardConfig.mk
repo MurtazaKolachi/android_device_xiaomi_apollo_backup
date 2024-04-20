0effdc60f750bcb423aa5ea9533a5136e64bdafc
johnmart19 <johnivan19999@gmail.com>
2024-04-20 04:13:22 +0300

    apollo: Camera: Enable newer HIDL overrideFormat (Another Implementation)
    Introduced in Evolution-X/hardware_interfaces@c7ca35486ad84c684b7909bc483241a16792e4d4
    
    Previously was enabled by default

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,8 @@ TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
 
 # Camera - HIDL overrideFormat
 TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT = true
+# Camera - HIDL overrideFormat - Alternative Version
+TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED = true
 
 # Display
 BOARD_USES_ADRENO := true
