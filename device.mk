0ba047b995e301ad271d789bb529b013f0d83599
Kelvin Zhang <zhangkelvin@google.com>
2024-09-15 01:17:26 +0800

    Apollo: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false
    
    Test: th
    Change-Id: I86a76296937a6abab1b4495cd66a35144348aafd
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- device.mk
+++ device.mk
@@ -316,6 +316,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 endif
 
+# Update
+AB_OTA_UPDATER := false
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.qti \
