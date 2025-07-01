f7358c6b916dd96456479981ebb09238ffffd97b
Murtaza <murtazakolachi7@gmail.com>
2025-07-01 21:03:54 +0500

    apollo: VoltageOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollo.mk
+    $(LOCAL_DIR)/voltage_apollo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollo-user \
-    lineage_apollo-userdebug \
-    lineage_apollo-eng
+    voltage_apollo-user \
+    voltage_apollo-userdebug \
+    voltage_apollo-eng
